86b1ebf48c9e9145f54aecb07931124375f1e141
fhzhangf <fhzhangf@isoftstone.com>
2022-09-21 16:32:59 +0800

    a conflict

--- services/users/project/test2.py
+++ services/users/project/test2.py
@@ -25,7 +25,7 @@ class Student(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    stu = Student('jettomerge', 59)
+    stu = Student('jetthisis a conflit', 59)
     db.session.add(stu)
     db.session.commit()
 
