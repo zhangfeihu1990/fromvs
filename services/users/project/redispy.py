7465c3b1d34e74e3e236b9d055252bb9f21a2721
fhzhangf <fhzhangf@isoftstone.com>
2022-09-21 13:57:11 +0800

    first commit

---
+++ services/users/project/redispy.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template 
+from redis import StrictRedis
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+baseURL = "mysql://root:123456@localhost/test"
+app.config['SQLALCHEMY_DATABASE_URI'] = baseURL #配置数据库url
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    age = db.Column(db.Integer, unique=True)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    stu = Student('jet Li', 58)
+    db.session.add(stu)
+    db.session.commit()
+
+    print('12345')
+    r= None 
+    try:
+        r = StrictRedis(host='redis01',decode_responses=True)
+        if not r.get("k1"):
+            r.set("k1",0)
+        r.incr("k1")
+        return render_template('index.html', num = r.get("k1"))
+    finally:
+        r.close()
